c641c72c021a82cddaa25c85921201cd8ce6d3ec
Aaron Marc Saunders <ams@bio.aau.dk>
2013-10-27 00:42:07 +0200

    Update utils.py
    os.walk corrected to os.path.walk

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 # http://stackoverflow.com/questions/2186525/use-a-glob-to-find-files-recursively-in-python
 
-import os.walk, fnmatch
+import os.path.walk, fnmatch
 
 def find_files(treeroot, pattern):
     results = []
-    for base, dirs, files in os.walk(treeroot):
+    for base, dirs, files in os.path.walk(treeroot):
         goodfiles = fnmatch.filter(files, pattern)
         results.extend(os.path.join(base, f) for f in goodfiles)
     return results
