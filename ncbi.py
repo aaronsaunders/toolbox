039e781e8f8184e9efd2c98f003c3300794c74f5
Aaron Saunders <aaron.marc.saunders@gmail.com>
2015-08-03 11:52:41 +0200

    Formatting and email update

--- ncbi.py
+++ ncbi.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
-# by Aaron (ams@bio.aau.dk)
+# by Aaron (amsa@baaa.dk)
 
 ########## Import functions ###########
 
-import sys 
+import sys
 from Bio import Entrez
 from  Bio.Blast  import  NCBIWWW
 from  Bio.Blast  import  NCBIXML
 import re
 from Bio import SeqIO
 
-
-Entrez.email = 'ams@bio.aau.dk'
-
+Entrez.email = 'amsa@baaa.dk'
 
 """
 A library of functions for efetch scripting
 """
-
-
 ############## Functions   ###############
 
 def parse_cds(record):
       """
       Takes Biopython SeqRecord as input
-      returns CDS acc, start, stop, strand
+      returns CDS tuple (acc, start, stop, strand)
       """
 
       # parse out coded_by qualifier from biopython SeqRecord
@@ -37,34 +33,34 @@ def parse_cds(record):
       if not coded_by:
           logfile.write('Error: CDS not found for %s\n'%record.id)
           cds_not_found.append(record.id)
-          return      
+          return
       if CDS_count > 1:
           logfile.write('Error: Multiple CDS found for %s\n'%record.id)
           cds_not_found.append(record.id)
           return
-      
+
       # accepts
-      # NM_010510.1:21..569 
+      # NM_010510.1:21..569
       # NM_010510.1<21..569
-      # NM_010510.1:21..>569  
+      # NM_010510.1:21..>569
       # NM_010510.1:<1..>569)
       # complement(NM_010510.1:21..569)
-      
+
       mobj = re.search('^complement\(([^)]+)\)', str(coded_by))
       if mobj:
           cds_strand = 'MINUS'
           coded_by = mobj.group(1)
       else:
           cds_strand = 'PLUS'
-      
+
       pattern = '(\w+\.\d):<?(\d+)\.\.>?(\d+)'
       mobj = re.search(pattern, str(coded_by))
       if mobj:
-          cds_acc = mobj.group(1) 
-          cds_start = mobj.group(2) 
-          cds_stop = mobj.group(3) 
+          cds_acc = mobj.group(1)
+          cds_start = mobj.group(2)
+          cds_stop = mobj.group(3)
 
-      return cds_acc, cds_start, cds_stop, cds_strand
+      return (cds_acc, cds_start, cds_stop, cds_strand)
 
 def  efetch_nt(acc, start=None, stop=None, strand=None):
     """Wrapper for Bio.efetch.
