3b3a6916d2ad13ac5ac0ff01beaa0fe2ce839697
Aaron Saunders <aaron.marc.saunders@gmail.com>
2015-08-03 12:16:11 +0200

    Reinstated the get_sizes function

--- bioplots.py
+++ bioplots.py
@@ -1,12 +1,37 @@
+#!/usr/bin/python
+# by Aaron (ams@bio.aau.dk)
+
+from Bio import SeqIO
+from toolbox import biofileformat
+import matplotlib.pyplot as plt
+
+def get_sizes(fname, sub=False):
+    """Parses a sequence file and returns a list of sequence lengths"""
+
+    with biofileformat.FileType('rb')(fname) as fh:
+        seqformat = biofileformat.from_handle(fh)
+
+        okformats = [ "fasta", "fastq" ]
+        if seqformat not in okformats:
+            print "takes only fasta/fastq w/wo compression"
+            return
+
+        if sub:
+            sizes = []
+            for n, rec in enumerate(SeqIO.parse(fh, seqformat)):
+                sizes.append(len(rec))
+                if n == (sub - 1):
+                    break
+        else:
+            sizes = [len(rec) for rec in SeqIO.parse(fh, seqformat)]
+
+    return sizes
+
 def plot_length(fname, sub=False):
     """
     Parses a sequence file and returns a plot of sequence lengths.
     Optional argument to subset the file.
-    
     """
-    import matplotlib.pyplot as plt
-    from toolbox.bioutils import get_sizes
-    
     sizes = get_sizes(fname, sub)
 
     plt.hist(sizes)
